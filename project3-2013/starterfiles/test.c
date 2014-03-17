54b75f9dc1b46069770578af824b6b8b72223b41
451user <451user@localhost.localdomain>
2014-03-16 21:32:56 -0700

    updated stuff so that it seeks properly

--- project3-2013/starterfiles/test.c
+++ project3-2013/starterfiles/test.c
@@ -16,7 +16,31 @@
 #define LOG_PRINTF(...)
 #endif
 
+struct ext2_super_block* get_super_block(int fd) {
+  struct ext2_super_block *ret = malloc(sizeof(struct ext2_super_block));
+  if (ret==NULL) return NULL;
+
+  lseek(fd, SUPERBLOCK_OFFSET, SEEK_SET);
+  int r = read(fd, ret, sizeof(struct ext2_super_block));
+
+  if (r != sizeof(struct ext2_super_block)){
+    printf("Error: could not read super block\n");
+    free (ret);
+    return NULL;
+  }
+
+  // sanity test
+  LOG_PRINTF("Should be one: %d\n",ret->s_first_data_block);
+  if (ret->s_magic != EXT2_SUPER_MAGIC)
+    LOG_PRINTF("This should not print\n");
+  
+  return ret;
+
+}
+
 int main(int argc, char* argv[]) {
+  int r;
+
   // check that input is correct
   if (argc != 2){
     printf("USAGE: need to give a filesystem to read\n");
@@ -27,22 +51,9 @@ int main(int argc, char* argv[]) {
   int fd = open(argv[1], O_RDONLY);
 
   //read in super_block
-  struct ext2_super_block *sb = malloc(sizeof(struct ext2_super_block));
-  if (sb==NULL) return -1;
-
-  lseek(fd, SUPERBLOCK_OFFSET, SEEK_SET);
-  int r = read(fd, sb, sizeof(struct ext2_super_block));
-
-  if (r != sizeof(struct ext2_super_block)){
-    printf("Error: could not read super block\n");
-    free (sb);
+  struct ext2_super_block *sb = get_super_block(fd);
+  if (sb == NULL)
     return -1;
-  }
-
-  // sanity test
-  LOG_PRINTF("Should be one: %d\n",sb->s_first_data_block);
-  if (sb->s_magic != EXT2_SUPER_MAGIC)
-    LOG_PRINTF("This should not print\n");
 
   // make recovered_files directory
   DIR* dir = opendir("recovered_files");
@@ -59,10 +70,12 @@ int main(int argc, char* argv[]) {
     return -1;
   }
 
+  // get name of specific filesystem directory
   char fs_dir[strlen(argv[1]) + 17];
   strcpy(fs_dir, "recovered_files/");
   strcat(fs_dir, argv[1]);
   LOG_PRINTF("%s\n", fs_dir);
+
   // make directory for specific filesystem
   dir = opendir(fs_dir);
   if (ENOENT == errno){
@@ -77,88 +90,120 @@ int main(int argc, char* argv[]) {
     return -1;
   }
 
+  // calculate block size
+  unsigned int block_size = 1024 << sb->s_log_block_size;
 
-  
+  // calculate number of block groups on the disk
+  unsigned int group_count = 1 + (sb->s_blocks_count-1) / sb->s_blocks_per_group;
+
+  // calculate size of the group descriptor list in bytes
+  unsigned int descr_list_size = group_count * sizeof(struct ext2_group_desc);
+
+  if (descr_list_size > block_size) {
+    LOG_PRINTF("group descriptor array is more than one block in size\n");
+  }
+
+  // get group descriptors
   struct ext2_group_desc *gd = malloc(sizeof(struct ext2_group_desc));
   if (gd == NULL) return -1;
 
-  r = read(fd, gd, sizeof(struct ext2_group_desc));
-  if (r != sizeof(struct ext2_group_desc)){
-    LOG_PRINTF("ERROR failed to read group desc\n");
-    return -1;
-  }
-
-  LOG_PRINTF("%d\n", gd->bg_inode_table);
-
-  int block_size = 1024 << sb->s_log_block_size;
-
-  LOG_PRINTF("bitmap: %d\n", gd->bg_inode_bitmap);
-  LOG_PRINTF("table: %d\n", gd->bg_inode_table);
-
-  lseek(fd, gd->bg_inode_bitmap * block_size, SEEK_SET);
-
-  LOG_PRINTF("num inodes: %d\n", sb->s_inodes_count);
-  
-
-  // number of bytes in the inode_bitmap
-  int bytes_to_read;
-
-  char buf[128];
-  int k = 0;
-  // WHY THIS IS STUPID AS SHIT. THERE AREN'T 16 bits per BYTE!!!!!!!!!!!
-  for (bytes_to_read = (sb->s_inodes_count/16) - 1; bytes_to_read > 0; bytes_to_read -= 128){
-    int buf_size;
-    if (bytes_to_read < 128)
-      buf_size = bytes_to_read;
-    else
-      buf_size = 128;
-    r = read(fd, buf, buf_size);
-    if (r != buf_size){
-      LOG_PRINTF("ERROR failed to read bitmap\n");
+  int a;
+  for (a = 0; a < group_count; a++){
+    lseek(fd, SUPERBLOCK_OFFSET + block_size + a*sizeof(struct ext2_group_desc), SEEK_SET);
+    r = read(fd, gd, sizeof(struct ext2_group_desc));
+    if (r != sizeof(struct ext2_group_desc)){
+      LOG_PRINTF("ERROR failed to read group desc\n");
       return -1;
     }
     
+    LOG_PRINTF("%d\n", gd->bg_inode_table);
     
-    /*
-      if (1 & buf[1])
-      LOG_PRINTF("first value is the right most bit of the byte\n");
-      else{
-      if (1<<7 & buf[1])
-      LOG_PRINTF("first value is the left most bit of the byte\n");
+    LOG_PRINTF("bitmap: %d\n", gd->bg_inode_bitmap);
+    LOG_PRINTF("table: %d\n", gd->bg_inode_table);
+
+    lseek(fd, gd->bg_inode_bitmap * block_size, SEEK_SET);
+    
+    LOG_PRINTF("num inodes: %d\n", sb->s_inodes_count);
+  
+
+    // number of bytes in the inode_bitmap
+    int bytes_to_read;
+
+    char buf[128];
+    int k = 0;
+    for (bytes_to_read = (sb->s_inodes_per_group/8); bytes_to_read > 0; bytes_to_read -= 128){
+      int buf_size;
+      if (bytes_to_read < 128)
+	buf_size = bytes_to_read;
       else
-      LOG_PRINTF("WTF!\n");
+	buf_size = 128;
+      r = read(fd, buf, buf_size);
+      if (r != buf_size){
+	LOG_PRINTF("ERROR failed to read bitmap\n");
+	return -1;
       }
-      if (1<<7 & buf[1])
-      LOG_PRINTF("first value is the left most bit of the byte\n");
-    */
-    int i, j;
-    for (j = 0; j < buf_size; j++){
-      for (i = 0; i < 8; i++){
-	int inode_num = i + (j*8) + (k*8);
-	if (1<<i & buf[j]){
-	  LOG_PRINTF("%d\n", i + (j*8) + (k*8));
-	  //	  if ((i+(j*8)+(k*8))>12) return -1;
-	}
+      
+      
+      /*
+	if (1 & buf[1])
+	LOG_PRINTF("first value is the right most bit of the byte\n");
 	else{
-	  // seek to free inode to see if it had previously been deleted
-	  lseek(fd, block_size * gd->bg_inode_table + (inode_num) * sizeof(struct ext2_inode), SEEK_SET);
-	  struct ext2_inode *inode = malloc(sizeof(struct ext2_inode));
-	  r = read(fd, inode, sizeof(struct ext2_inode));
-	  if (r != sizeof(struct ext2_inode)){
-	    LOG_PRINTF("ERROR failed to read root");
-	    return -1;
+	if (1<<7 & buf[1])
+	LOG_PRINTF("first value is the left most bit of the byte\n");
+	else
+	LOG_PRINTF("WTF!\n");
+	}
+	if (1<<7 & buf[1])
+	LOG_PRINTF("first value is the left most bit of the byte\n");
+      */
+      int i, j;
+      for (j = 0; j < buf_size; j++){
+	for (i = 0; i < 8; i++){
+	  int inode_num = i + (j*8) + (k*8);
+	  if (1<<i & buf[j]){
+	    LOG_PRINTF("%d\n", i + (j*8) + (k*8));
+	    //	  if ((i+(j*8)+(k*8))>12) return -1;
 	  }
-	  if (inode->i_dtime > 0){
-	    // create a file, write deleted contents to it 
-	    restore_file(inode);
+	  else{
+	    // seek to free inode to see if it had previously been deleted
+	    lseek(fd, (block_size * gd->bg_inode_table) + (inode_num * sizeof(struct ext2_inode)), SEEK_SET);
+	    struct ext2_inode *inode = malloc(sizeof(struct ext2_inode));
+	    r = read(fd, inode, sizeof(struct ext2_inode));
+	    if (r != sizeof(struct ext2_inode)){
+	      LOG_PRINTF("ERROR failed to read root");
+	      return -1;
+	    }
+	    if (inode->i_dtime > 0){
+	      LOG_PRINTF("i_blocks: %d\n", inode->i_blocks);
+	      LOG_PRINTF("inode size: %d\n", inode->i_size);
+	      LOG_PRINTF("first data block: %d\n", inode->i_block[0]);
+
+	      // create a file, write deleted contents to it 
+	      //	    restore_file(inode);
+	      int x;
+	      for(x = 0;  x < inode->i_blocks/(block_size/512); x++){
+		LOG_PRINTF("TEST\n");	
+		int ppb = block_size/4;
+		LOG_PRINTF("ppb: %d\n", ppb);
+		char pointer[4];
+		char buffer[128];
+
+		lseek(fd, block_size * inode->i_block[x], SEEK_SET);
+		r = read(fd, buffer, 127);
+		buffer[127] = '\0';
+		LOG_PRINTF("data: %s", buffer);
+		LOG_PRINTF("inode to be restored: %d\n", inode_num);
+	      }
+
+	    }
+	      // assume inodes are assigned sequentially
+	    else
+	      break;
 	  }
-	  // assume inodes are assigned sequentially
-	  else
-	    break;
 	}
       }
+      k += 128;
     }
-    k += 128;
   }
 
   LOG_PRINTF("first good inode:%d\n", EXT2_FIRST_INO(sb));
